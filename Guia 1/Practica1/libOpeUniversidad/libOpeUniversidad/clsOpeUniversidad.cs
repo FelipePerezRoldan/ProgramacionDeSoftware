b0c47dff2af41fe91d200ce89f7e7df31c83b4ae
Wolfang Pérez <wolfangperez302437@correo.itm.edu.co>
2023-08-13 14:15:26 -0500

    MiPrimerCommit

---
+++ Guia 1/Practica1/libOpeUniversidad/libOpeUniversidad/clsOpeUniversidad.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace libOpeUniversidad
+{
+    public class clsOpeUniversidad
+    {
+        #region "Atributos"
+        private int intTipoEst;
+        private float fltProm;
+        private float fltValorCred;
+        private int intCredit;
+        private float fltValDescuento;
+        private float fltValorPago;
+        private string strError;
+        #endregion
+        #region "Constructor"
+        public clsOpeUniversidad()
+        {
+            intTipoEst = 0;
+            fltValorCred = 0;
+            intCredit = 0;
+            fltValDescuento = 0;
+            fltValorPago = 0;
+            strError = string.Empty;
+        }
+        #endregion
+        #region "Propiedades"
+        public int tipoEstudiante
+        {
+            get { return intTipoEst; }
+            set { intTipoEst = value; }
+        }
+        public float Promedio
+        {
+            get { return fltProm; }
+            set { fltProm = value; }
+        }
+        public float valorCred
+        {
+            set { fltValorCred = value; }
+        }
+        public int numCreditos
+        {
+            set { intCredit = value; }
+        }
+        public float Descuento
+        {
+            set { fltValDescuento = value; }
+        }
+        public float valorPago
+        {
+            set { fltValorPago = value; }
+        }
+        public string Error
+        {
+            set { strError = value; }
+        }
+        #endregion
+        #region "Metodo público"
+        public bool hallarPago()
+        {
+            return hallarPago();
+        }
+        #endregion
+    }
+}
---
+++ Guia 1/Practica1/libRnUniversidad/libRnUniversidad/clsRnUniversidad.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO; //quitar los errores de el metodo leer achivo, libreria para cargar un archivo de tipo plano
+
+
+namespace libRnUniversidad
+{
+    public class clsRnUniversidad
+    {
+        #region "Atributos"
+        private int intTipoEst;
+        private float fltProm;
+        private float fltValCredito;
+        private int intCredit;
+        private float fltDesc;
+        private string strError;
+        #endregion
+
+        #region "Constructor"
+        public clsRnUniversidad()
+        {
+            intCredit = 0;
+            fltProm = 0;
+            fltValCredito = 0;
+            intCredit = 0;
+            fltDesc = 0;
+            strError = string.Empty; // = ""
+        }
+        #endregion
+        #region "Propiedades"
+        //Entrada
+        public int tipoEstudiante
+        {
+            set { intTipoEst = value; }
+        }
+
+        public float Promedio
+        {
+            set { fltProm = value; }
+        }
+
+        //Salida
+        public float ValorCredito
+        {
+            get { return fltValCredito; }
+        }
+
+        public int numCreditos
+        {
+            get { return intCredit; }
+        }
+
+        public float Descuento
+        {
+            get { return fltDesc; }
+        }
+
+        public string error
+        {
+            get { return strError; }
+        }
+        #endregion
+        #region "Metodos privados"
+        private bool Validar()
+        {
+            if (intTipoEst < 1 || intTipoEst > 2)  //intTipioEst != 1 && intTipioEst != 2
+            {
+                strError = "Tipo de estudiante no válido";
+                return false;
+            }
+            if (fltProm < 0 || fltProm > 5)
+            {
+                strError = "Promedio de nota no válido";
+                return false;
+            }
+            return true;
+        }
+        private bool leerArchivo()
+        {
+            if (!Validar())
+                return false;
+            try
+            {
+                string strPath = AppDomain.CurrentDomain.BaseDirectory + @"Descuentos.txt";
+                int intCant = 0;
+                string strLinea;
+                string[] vectorLinea;
+                string strCodigo;
+                float fltNota;
+                intCant = File.ReadAllLines(strPath).Length;
+                if (intCant <= 0)
+                    return true;
+                StreamReader Archivo = new StreamReader(@strPath); //Crear objeto para leer el archivo
+                while ((strLinea = Archivo.ReadLine()) != null)      //Leer Linea * Linea el archivo
+                {
+                    vectorLinea = strLinea.Split(':');
+                    strCodigo = vectorLinea[0];                        //Tipo de estudiante (Programa)
+                    fltNota = Convert.ToSingle(vectorLinea[1]);        //Promedio mínimo de Nota
+                    if (strCodigo == intTipoEst.ToString() && fltProm >= fltNota)
+                    {
+                        fltValCredito = Convert.ToSingle(vectorLinea[2]);  //Valor crédito
+                        intCredit = Convert.ToInt16(vectorLinea[3]);  //Cantidad Créditos
+                        fltDesc = Convert.ToSingle(vectorLinea[4]);  //Porcentaje Dscto
+                    }
+                }
+                Archivo.Close();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                strError = ex.Message;
+                return false;
+            }
+        }
+        #endregion
+        #region "Metodos publicos"
+        public bool hallarDatos()
+        {
+            return leerArchivo(); //Encapsulamiento
+        }
+        #endregion
+    }
+}
+
