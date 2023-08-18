b5e9241ef137f36155130514f90ed5d7ad07108e
Wolfang Pérez <wolfangperez302437@correo.itm.edu.co>
2023-08-18 10:32:31 -0500

    Actualización con clase desatraso

--- Guia 1/Practica1/appPractica1/appPractica1/Form1.Designer.cs
+++ Guia 1/Practica1/appPractica1/appPractica1/Form1.Designer.cs
@@ -29,95 +29,99 @@ namespace appPractica1
         /// </summary>
         private void InitializeComponent()
         {
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.button2 = new System.Windows.Forms.Button();
+            this.cmbTipoEst = new System.Windows.Forms.ComboBox();
+            this.txtCarne = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.txtPromedio = new System.Windows.Forms.TextBox();
+            this.btnProcesar = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
-            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.txtMsj = new System.Windows.Forms.TextBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             this.textBox8 = new System.Windows.Forms.TextBox();
             this.textBox7 = new System.Windows.Forms.TextBox();
             this.textBox6 = new System.Windows.Forms.TextBox();
-            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.txtnroCred = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
-            this.label8 = new System.Windows.Forms.Label();
             this.label9 = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // comboBox1
+            // cmbTipoEst
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(130, 56);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 21);
-            this.comboBox1.TabIndex = 0;
+            this.cmbTipoEst.FormattingEnabled = true;
+            this.cmbTipoEst.Items.AddRange(new object[] {
+            "Seleccione un tipo de Estudiante",
+            "Pregrado",
+            "Postgrado"});
+            this.cmbTipoEst.Location = new System.Drawing.Point(148, 75);
+            this.cmbTipoEst.Name = "cmbTipoEst";
+            this.cmbTipoEst.Size = new System.Drawing.Size(345, 21);
+            this.cmbTipoEst.TabIndex = 0;
             // 
-            // textBox1
+            // txtCarne
             // 
-            this.textBox1.Location = new System.Drawing.Point(130, 83);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 1;
+            this.txtCarne.Location = new System.Drawing.Point(148, 102);
+            this.txtCarne.Name = "txtCarne";
+            this.txtCarne.Size = new System.Drawing.Size(345, 20);
+            this.txtCarne.TabIndex = 1;
             // 
-            // textBox2
+            // txtNombre
             // 
-            this.textBox2.Location = new System.Drawing.Point(130, 109);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
-            this.textBox2.TabIndex = 2;
+            this.txtNombre.Location = new System.Drawing.Point(148, 128);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(345, 20);
+            this.txtNombre.TabIndex = 2;
             // 
-            // button1
+            // btnLimpiar
             // 
-            this.button1.Location = new System.Drawing.Point(130, 167);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "Limpiar";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Location = new System.Drawing.Point(148, 186);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpiar.TabIndex = 3;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
             // 
-            // textBox3
+            // txtPromedio
             // 
-            this.textBox3.Location = new System.Drawing.Point(130, 141);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 20);
-            this.textBox3.TabIndex = 4;
+            this.txtPromedio.Location = new System.Drawing.Point(148, 160);
+            this.txtPromedio.Name = "txtPromedio";
+            this.txtPromedio.Size = new System.Drawing.Size(345, 20);
+            this.txtPromedio.TabIndex = 4;
             // 
-            // button2
+            // btnProcesar
             // 
-            this.button2.Location = new System.Drawing.Point(241, 167);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 5;
-            this.button2.Text = "Procesar";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnProcesar.Location = new System.Drawing.Point(259, 186);
+            this.btnProcesar.Name = "btnProcesar";
+            this.btnProcesar.Size = new System.Drawing.Size(75, 23);
+            this.btnProcesar.TabIndex = 5;
+            this.btnProcesar.Text = "Procesar";
+            this.btnProcesar.UseVisualStyleBackColor = true;
+            this.btnProcesar.Click += new System.EventHandler(this.btnProcesar_Click);
             // 
             // button3
             // 
             this.button3.Location = new System.Drawing.Point(323, 415);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.Size = new System.Drawing.Size(152, 39);
             this.button3.TabIndex = 6;
             this.button3.Text = "Terminar";
             this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.button3.Click += new System.EventHandler(this.btnTerminar);
             // 
-            // textBox4
+            // txtMsj
             // 
-            this.textBox4.Location = new System.Drawing.Point(151, 373);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(224, 20);
-            this.textBox4.TabIndex = 7;
-            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            this.txtMsj.Location = new System.Drawing.Point(151, 373);
+            this.txtMsj.Name = "txtMsj";
+            this.txtMsj.Size = new System.Drawing.Size(224, 20);
+            this.txtMsj.TabIndex = 7;
             // 
             // groupBox1
             // 
@@ -128,12 +132,49 @@ namespace appPractica1
             this.groupBox1.Controls.Add(this.textBox8);
             this.groupBox1.Controls.Add(this.textBox7);
             this.groupBox1.Controls.Add(this.textBox6);
-            this.groupBox1.Controls.Add(this.textBox5);
+            this.groupBox1.Controls.Add(this.txtnroCred);
             this.groupBox1.Location = new System.Drawing.Point(32, 210);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(432, 145);
             this.groupBox1.TabIndex = 8;
             this.groupBox1.TabStop = false;
+            this.groupBox1.Visible = false;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(266, 61);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(54, 13);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Vr. Dscto:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(264, 22);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(59, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Vr. Crédito:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(260, 107);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(63, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Vr. a Pagar:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(33, 22);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(71, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Nro. Créditos:";
             // 
             // textBox8
             // 
@@ -156,27 +197,26 @@ namespace appPractica1
             this.textBox6.Size = new System.Drawing.Size(100, 20);
             this.textBox6.TabIndex = 1;
             // 
-            // textBox5
+            // txtnroCred
             // 
-            this.textBox5.Location = new System.Drawing.Point(110, 19);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(100, 20);
-            this.textBox5.TabIndex = 0;
+            this.txtnroCred.Location = new System.Drawing.Point(110, 19);
+            this.txtnroCred.Name = "txtnroCred";
+            this.txtnroCred.Size = new System.Drawing.Size(100, 20);
+            this.txtnroCred.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(29, 59);
+            this.label1.Location = new System.Drawing.Point(47, 78);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(95, 13);
             this.label1.TabIndex = 9;
             this.label1.Text = "Tipo de estudiante";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(89, 86);
+            this.label2.Location = new System.Drawing.Point(107, 105);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(35, 13);
             this.label2.TabIndex = 10;
@@ -185,7 +225,7 @@ namespace appPractica1
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(80, 112);
+            this.label3.Location = new System.Drawing.Point(98, 131);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(44, 13);
             this.label3.TabIndex = 11;
@@ -194,54 +234,19 @@ namespace appPractica1
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(73, 144);
+            this.label4.Location = new System.Drawing.Point(91, 163);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(51, 13);
             this.label4.TabIndex = 12;
             this.label4.Text = "Promedio";
             // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(33, 22);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(71, 13);
-            this.label5.TabIndex = 4;
-            this.label5.Text = "Nro. Créditos:";
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(260, 107);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(63, 13);
-            this.label6.TabIndex = 5;
-            this.label6.Text = "Vr. a Pagar:";
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(264, 22);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(59, 13);
-            this.label7.TabIndex = 6;
-            this.label7.Text = "Vr. Crédito:";
-            // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(266, 61);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(54, 13);
-            this.label8.TabIndex = 7;
-            this.label8.Text = "Vr. Dscto:";
-            // 
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(211, 27);
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(169, 25);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(93, 13);
+            this.label9.Size = new System.Drawing.Size(229, 31);
             this.label9.TabIndex = 13;
             this.label9.Text = "Regla de Negocio";
             // 
@@ -249,21 +254,21 @@ namespace appPractica1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(500, 462);
+            this.ClientSize = new System.Drawing.Size(545, 513);
             this.Controls.Add(this.label9);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.groupBox1);
-            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.txtMsj);
             this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.btnProcesar);
+            this.Controls.Add(this.txtPromedio);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.txtCarne);
+            this.Controls.Add(this.cmbTipoEst);
             this.Name = "Form1";
             this.Text = "Form1";
             this.groupBox1.ResumeLayout(false);
@@ -275,19 +280,19 @@ namespace appPractica1
 
         #endregion
 
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox cmbTipoEst;
+        private System.Windows.Forms.TextBox txtCarne;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.TextBox txtPromedio;
+        private System.Windows.Forms.Button btnProcesar;
         private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox txtMsj;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.TextBox textBox8;
         private System.Windows.Forms.TextBox textBox7;
         private System.Windows.Forms.TextBox textBox6;
-        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox txtnroCred;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label5;
--- Guia 1/Practica1/appPractica1/appPractica1/Form1.cs
+++ Guia 1/Practica1/appPractica1/appPractica1/Form1.cs
@@ -16,20 +16,82 @@ namespace appPractica1
         {
             InitializeComponent();
         }
-
-        private void label1_Click(object sender, EventArgs e)
+        #region Metodos Propios
+        private void Mensaje (String texto)
         {
+            this.txtMsj.Text = texto;
+        }  
+        private void llenarCombo()
+        {
+            this.cmbTipoEst.Items.Add("Seleccione un tipo de estudiante"); //Index=0
+            this.cmbTipoEst.Items.Add("Pregrado");//Index=1
+            this.cmbTipoEst.Items.Add("Postgrado");//Index=2
+            this.cmbTipoEst.SelectedIndex = 0;
+        }
+        private void limpiar()
+        {
+            this.txtMsj.Text = string.Empty;
+            this.cmbTipoEst.SelectedIndex = 0;
+            this.txtCarne.Text = string.Empty;
+            this.txtNombre.Text = string.Empty;
+            this.txtPromedio.Text = string.Empty;
+            this.txtnroCred.Text = string.Empty;
+            this.txtCarne.Text = string.Empty;
+            this.txtCarne.Text = string.Empty;
+            this.txtCarne.Text = string.Empty;
+            this.groupBox1.Visible = false;
+            this.cmbTipoEst.Focus();
+        }
+        #endregion
 
+        private void btnTerminar(object sender, EventArgs e)
+        {
+            this.Close();
         }
 
-        private void textBox4_TextChanged(object sender, EventArgs e)
+        private void btnProcesar_Click(object sender, EventArgs e)
         {
+            int carne, tipoE;
+            String nombre;
+            float pNota;
+            try
+            {
+                Mensaje("");
+                //caputura de datos
+                tipoE = this.cmbTipoEst.SelectedIndex;
+                if (tipoE <= 0)
+                {
+                    Mensaje("Tipo de estudiante no valido");
+                    this.cmbTipoEst.Focus();
+                    return;
+                }
+                nombre = this.txtNombre.Text.Trim();
+                carne = Convert.ToInt16(this.txtCarne.Text);
+                pNota = Convert.ToSingle(this.txtPromedio.Text);
+                //crear el objeto
+                clsOpeUniversidad oP = new clsOpeUniversidad();
+                //enviar info
+                oP.tipoestudiante = tipoE;
+                //invocar el metodo y tratamiento del error
+                if (!oP.hallarPago())
+                {
+                    Mensaje(oP.Error);
+                    oP = null;
+                    return;
+                }
+                //recuperar info
+                this.txtnroCred.Text = oP.numCreditos.ToString();
+                this.txtVrCred.Text = oP.valorCredito.ToString();
+                this.txtDscto.Text = oP.Descuento.ToString();
+                this.VrPagar.Text = oP.valorPago.ToString();
+                oP = null;
+                this.gpbRpta.Visible = true;
+            }
+            catch (Exception ex)
+            {
 
-        }
-
-        private void button3_Click(object sender, EventArgs e)
-        {
-
+                Mensaje(ex.Message);
+            }
         }
     }
 }
